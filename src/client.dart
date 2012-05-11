d102d7cbdf25d59e0185e465f9d1580e6bd294e5
Nicholas Ng <nickng@projectfate.org>
2012-05-12 00:28:50 +0100

    Example client and client.dart backbone

--- src/client.dart
+++ src/client.dart
@@ -4,3 +4,39 @@
  *
  * Connect to server via websocket and handle push messages from server.
  */
+
+#import('dart:html');
+#import('dart:io');
+#import('dart:json');
+
+class PresendartClient {
+
+  bool _sync;
+  Socket _socket;
+
+  void connect() {
+    String host = document.query('#host_id').attributes['value'];
+    int port = Math.parseInt(document.query('#port_id').attributes['value']);
+    Element canvas = document.query('#canvas');
+    canvas.innerHTML = 'Connecting to $host:$port.. (Slide goes here)';
+
+    _socket = new Socket(host, port);
+
+    _socket.onData = () {
+      Map data = JSON.parse(_socket.inputStream.read().toString());
+      updateSlides(data);
+    };
+  }
+
+  void updateSlides(Map data) {
+  }
+
+}
+
+void main()
+{
+  PresendartClient pc = new PresendartClient();
+  document.query('#startbutton').on.click.add(
+    (e) { pc.connect(); }
+  );
+}
